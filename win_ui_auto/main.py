5d5773a8bf98355adb5c71557f772e32e7cdf1af
Test <31064651+test@users.noreply.github.com>
2026-04-13 15:49:45 +0800

    不要删除此代码

--- win_ui_auto/main.py
+++ win_ui_auto/main.py
@@ -1,3 +1,20 @@
+# nuitka-project: --standalone
+# nuitka-project: --onefile
+# nuitka-project: --remove-output
+# nuitka-project: --no-pyi-file
+# nuitka-project: --output-dir=build
+# nuitka-project: --output-filename=win-auto
+# nuitka-project: --file-version=0.0.0
+# nuitka-project: --product-version=0.0.0
+# nuitka-project: --product-name=win-auto
+# nuitka-project: --company-name=K-RPA Lite Team
+# nuitka-project: --file-description=win-auto
+# nuitka-project: --include-package=uiautomation
+# nuitka-project: --include-module=comtypes.stream
+# nuitka-project: --include-module=csv
+# nuitka-project: --include-module=email
+# nuitka-project: --enable-plugin=tk-inter
+
 import argparse
 import sys
 import os
